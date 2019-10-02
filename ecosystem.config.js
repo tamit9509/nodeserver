0605c993081fb1626200a19e46ba2e132ed2f0e1
Amit Thakur <tamit9509@gail.com>
2019-10-02 15:44:11 +0530

    - add configuration for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [{
+    name: 'ChatApp APIs',
+    script: 'src/server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '15.206.24.203',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
